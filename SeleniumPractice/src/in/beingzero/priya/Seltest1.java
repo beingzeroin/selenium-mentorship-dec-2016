8bbf33fda49d397d08be86270e7f7ca6449ba0e0
priya <priyataran@gmail.com>
2016-12-19 16:41:45 +0530

    Test Case 1 getting an error "The path to the driver executable must be set by the webdriver.gecko.driver system property;"

--- SeleniumPractice/src/in/beingzero/priya/Seltest1.java
+++ SeleniumPractice/src/in/beingzero/priya/Seltest1.java
@@ -1,10 +1,40 @@
 package in.beingzero.priya;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Seltest1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args)
+	
+	{
+		
+	WebDriver driver = new FirefoxDriver();
+	
+	driver.get("http://opensource.demo.orangehrmlive.com");
+	WebElement UserId = driver.findElement(By.id("txtUsername"));
+	UserId.sendKeys("Admin");
+	
+	WebElement Password = driver.findElement(By.id("txtPassword"));
+	Password.sendKeys("admin");
+	
+	WebElement Login = driver.findElement(By.id("btnLogin"));
+	Login.click();
+	
+	String Actuallogger = driver.findElement(By.id("welcome")).getText();
+	
+	String Expectedlogger = "Welcome Admin";
+	
+	Assert.assertEquals(Expectedlogger, Actuallogger);
+	System.out.println("Successfully login into OrangeHRM by Admin");
+	driver.close();
+	
+	
+	
 	}
 
 }
