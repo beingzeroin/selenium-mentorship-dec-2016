2f1319110a77f11a5b39d7dee479b1719c8aa95d
Ravi <ravi.dubey13@gmail.com>
2016-12-20 22:57:38 +0530

    Changes to class Path

--- SeleniumPractice/src/in/beingzero/Ravi/LogInOhrm.java
+++ SeleniumPractice/src/in/beingzero/Ravi/LogInOhrm.java
@@ -1,13 +1,14 @@
 package in.beingzero.Ravi;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LogInOhrm {
 
 	// Test One - Log in with Valid UserName and PassWord
 	public void VuserAndVpwd() {
 		System.out.println("Test - Log in with Valid Credentials");
-		FirefoxDriver browser = new FirefoxDriver();
+		WebDriver browser = new ChromeDriver();
 		browser.get("http://opensource.demo.orangehrmlive.com");
 
 	}
