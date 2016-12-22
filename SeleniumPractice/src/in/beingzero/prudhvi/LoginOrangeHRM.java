4b6075a1ffdbb963a3de65841e6109dcbd08d245
tammanap <tammanap@gmail.com>
2016-12-22 08:42:22 +0530

    nothing

--- SeleniumPractice/src/in/beingzero/prudhvi/LoginOrangeHRM.java
+++ SeleniumPractice/src/in/beingzero/prudhvi/LoginOrangeHRM.java
@@ -1,6 +1,6 @@
 package in.beingzero.prudhvi;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginOrangeHRM {
