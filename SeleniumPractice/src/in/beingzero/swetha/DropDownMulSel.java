80acc73d6ee6c0058a489ad81ce95df947e491b3
sswethajain <sswethajain@gmail.com>
2016-12-27 17:32:41 +0000

    Action Item -16

---
+++ SeleniumPractice/src/in/beingzero/swetha/DropDownMulSel.java
@@ -0,0 +1,15 @@
+package in.beingzero.swetha;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DropDownMulSel {
+
+	public static void main(String[] args) {
+		FirefoxDriver browser = new FirefoxDriver();
+		 
+	       // Go to BeingZero Home Page.
+	       browser.get("http://beingzero.in/our-team");
+
+	}
+
+}
