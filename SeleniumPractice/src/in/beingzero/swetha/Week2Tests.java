efd3a033079069ccacc98cd17ddb19548ad38a21
sswethajain <sswethajain@gmail.com>
2016-12-28 12:44:36 +0000

    settings

--- SeleniumPractice/src/in/beingzero/swetha/Week2Tests.java
+++ SeleniumPractice/src/in/beingzero/swetha/Week2Tests.java
@@ -1,10 +1,15 @@
 package in.beingzero.swetha;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Week2Tests {
 
 	public static void main(String[] args) {
 		System.out.println("Verification passed");
+		FirefoxDriver fd = new FirefoxDriver();
 
+		fd.get("http://opensource.demo.orangehrmlive.com");
 	}
 
 }
