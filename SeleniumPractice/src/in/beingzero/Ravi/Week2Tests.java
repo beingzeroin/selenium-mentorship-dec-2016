bae147666a6d9343d6e1e3c6d9f85a78cf385f19
Ravi <ravi.dubey13@gmail.com>
2016-12-30 00:07:45 +0530

    unused calls removed

--- SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
+++ SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Week2Tests {
 		//public void verifyAboutCompanyNameContainsOrangeHRM() {
