6cff76ff873bb3a2b8a9f4a1d2f7a012f08d57e7
Ravi <ravi.dubey13@gmail.com>
2016-12-30 14:00:47 +0530

    Test inprogress

--- SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
+++ SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Week2Tests {
@@ -17,7 +18,7 @@ public class Week2Tests {
 	public void verifyLogoutWorks() {
 		System.out.println("Test - Log in with Valid Credentials");
 
-		WebDriver browser = new ChromeDriver();
+		WebDriver browser = new FirefoxDriver();
 		browser.get("http://opensource.demo.orangehrmlive.com");
 		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		// browser.close();
@@ -28,17 +29,11 @@ public class Week2Tests {
 		WebElement password = browser.findElement(By.id("txtPassword"));
 		password.sendKeys("admin");
 
-		// org.openqa.selenium.Alert alert = browser.switchTo().alert();
-		// ((org.openqa.selenium.Alert) alert).dismiss();
-
-		// browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		// browser.switchTo().window("handle");
-
 		WebElement login = browser.findElement(By.id("btnLogin"));
 		login.click();
 		WebElement wlcm = browser.findElement(By.id("welcome"));
 		wlcm.click();
-		WebElement logout = browser.findElement(By.xpath("/index.php/auth/logout"));
+		WebElement logout = browser.findElement(By.xpath(".//*[@id='welcome-menu']/ul/li[2]/a"));
 		logout.click();
 	}
 	}
