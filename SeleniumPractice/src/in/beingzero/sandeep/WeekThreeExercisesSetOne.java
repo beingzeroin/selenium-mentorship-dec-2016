af60e768daa4b9a226670465364aaf4b76d0ad67
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2016-12-31 20:39:42 +0530

    Week3 First Discussion and Exercises

---
+++ SeleniumPractice/src/in/beingzero/sandeep/WeekThreeExercisesSetOne.java
@@ -0,0 +1,26 @@
+package in.beingzero.sandeep;
+
+public class WeekThreeExercisesSetOne {
+
+	public static void main(String[] args) {
+		// Open OrageHRM
+		
+		// Click Directory Menu
+		
+		// Test 1
+		// Fill SEarch Term
+		// Select any item from dropdown of Job Title
+		// Select any item from dropwdown of locaton
+		// Click Reset
+		// Verify that Textbox is clear and dropdown selection are back to original
+		
+		
+		// Test 2
+		// Select a job title from dropdown
+		// Verify that all the table resutls are having seleced job title
+		
+		
+		
+	}
+	
+}
