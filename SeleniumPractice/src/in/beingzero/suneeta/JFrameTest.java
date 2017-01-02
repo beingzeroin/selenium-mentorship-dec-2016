8742665a2fdf964d0027ab3f97415f326f2a2347
Suneeta <sisuk@192.168.0.3>
2017-01-02 21:01:49 +0000

    JFrametest

---
+++ SeleniumPractice/src/in/beingzero/suneeta/JFrameTest.java
@@ -0,0 +1,18 @@
+package in.beingzero.suneeta;
+
+import javax.swing.JFrame;
+
+public class JFrameTest {
+	public static void main (String[] args) {
+		
+		JFrame jf = new JFrame();
+		
+		jf.setBounds(400,0,600 ,700);
+		
+		jf.setTitle("My First Title-Frame");
+		
+		jf.setVisible(true);
+		
+	}
+	
+}
