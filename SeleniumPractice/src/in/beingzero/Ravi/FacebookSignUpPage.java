d1fa7fd38ab0a8aa5ad1124aefa194eb9cf97e0a
Ravi <ravi.dubey13@gmail.com>
2017-01-02 22:15:13 +0530

    FB - DropDown and radio button

--- SeleniumPractice/src/in/beingzero/Ravi/FacebookSignUpPage.java
+++ SeleniumPractice/src/in/beingzero/Ravi/FacebookSignUpPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class FacebookSignUpPage {
 
@@ -20,7 +21,12 @@ public class FacebookSignUpPage {
 		WebElement conmob = ch.findElement(By.name("reg_email_confirmation__"));
 		WebElement pwd = ch.findElement(By.name("reg_passwd__"));
 		WebElement male = ch.findElement(By.id("u_0_i"));
-		
+		WebElement dob = ch.findElement(By.id("day"));
+		Select d = new Select(dob);
+		WebElement mob1 = ch.findElement(By.id("month"));
+		Select m = new Select(mob1);
+		WebElement yob = ch.findElement(By.id("year"));
+		Select y = new Select(yob);		
 		un.clear();
 		un.sendKeys("Ravi");
 		uln.clear();
@@ -32,6 +38,9 @@ public class FacebookSignUpPage {
 		pwd.clear();
 		pwd.sendKeys("test");
 		male.click();
+		d.selectByVisibleText("9");
+		m.selectByVisibleText("Jun");
+		y.selectByVisibleText("1986");
 	}
 
 }
