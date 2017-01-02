c9d450469b3310f2ce4f5742440ec5ea0e4bb044
Ravi <ravi.dubey13@gmail.com>
2017-01-02 18:36:54 +0530

    Being Zero logo

---
+++ SeleniumPractice/src/in/beingzero/Ravi/BeingZeroLogo.java
@@ -0,0 +1,22 @@
+package in.beingzero.Ravi;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BeingZeroLogo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		WebDriver ch = new ChromeDriver();
+		ch.get("http://beingzero.in/");
+		Thread.sleep(4000);
+		WebElement logo = ch.findElement(By.xpath("//div[@id='branding']//img[@class='preload-me']"));
+		System.out.println("location: " + logo.getLocation());
+		System.out.println("Dimension: " + logo.getSize());
+		ch.quit();
+
+	}
+
+}
