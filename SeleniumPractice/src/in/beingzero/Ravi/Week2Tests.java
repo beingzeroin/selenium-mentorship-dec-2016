0c6f94c138b8be735559754f0fe95c3b33f80056
Ravi <ravi.dubey13@gmail.com>
2017-01-04 21:49:49 +0530

    commit for minor changes

--- SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
+++ SeleniumPractice/src/in/beingzero/Ravi/Week2Tests.java
@@ -11,8 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Week2Tests {
 	public void verifyAboutCompanyNameContainsOrangeHRM() {
 		WebDriver browser = new ChromeDriver();
-		browser.get("http://opensource.demo.orangehrmlive.com");
 		browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		browser.manage().window().maximize();
+		browser.get("http://opensource.demo.orangehrmlive.com");
 		WebElement username = browser.findElement(By.id("txtUsername"));
 		username.sendKeys("Admin");
 
@@ -34,8 +35,9 @@ public class Week2Tests {
 		System.out.println("Test - Log in with Valid Credentials");
 
 		WebDriver browser = new ChromeDriver();
+		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		browser.manage().window().maximize();
 		browser.get("http://opensource.demo.orangehrmlive.com");
-		browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		WebElement username = browser.findElement(By.id("txtUsername"));
 		username.sendKeys("Admin");
 
@@ -46,7 +48,7 @@ public class Week2Tests {
 		login.click();
 		WebElement wlcm = browser.findElement(By.id("welcome"));
 		wlcm.click();
-		WebElement logout = browser.findElement(By.xpath(".//div[@id='welcome-menu']//a[contains(text(),'Logout')]"));
+		WebElement logout = browser.findElement(By.xpath("//*[@id='welcome-menu']/ul/li[2]/a"));
 		logout.click();
 		browser.quit();
 	}
