7f299c0ddb4b1d25ad9e8004bbb983a7f8899a2b
sswethajain <sswethajain@gmail.com>
2017-01-08 10:52:20 +0000

    Week3Test5 successful

--- SeleniumPractice/src/in/beingzero/swetha/Week3EmployeeTests.java
+++ SeleniumPractice/src/in/beingzero/swetha/Week3EmployeeTests.java
@@ -1,5 +1,6 @@
 package in.beingzero.swetha;
 
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -8,8 +9,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -359,12 +358,12 @@ public class Week3EmployeeTests {
 
 	public void deleteEmployeeTest() {
 
-//1.   Launch Browser.
-//2.   Open http://opensource.demo.orangehrmlive.com/
-//3.   Enter username - Admin
-//4.   Enter password - admin
-//5.   Click Login Button
-		
+		// 1.   Launch Browser.
+		// 2.   Open http://opensource.demo.orangehrmlive.com/
+		// 3.   Enter username - Admin
+		// 4.   Enter password - admin
+		// 5.   Click Login Button
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -380,7 +379,7 @@ public class Week3EmployeeTests {
 		WebElement Login = driver.findElement(By.id(ID3));
 		Login.click();
 
-// 6.   Verify that login succeeds and we then go to the PIM Page.
+		// 6.   Verify that login succeeds and we then go to the PIM Page.
 
 		actualHomePageText = driver.findElement(By.id("welcome")).getText();
 
@@ -391,46 +390,48 @@ public class Week3EmployeeTests {
 			WebElement tabPIM = driver.findElement(By.linkText("PIM"));
 			tabPIM.click();
 
-// 7.   Click on the Employee List SubMenu and Print out the URL
+			// 7.   Click on the Employee List SubMenu and Print out the URL
 
 			WebElement subMenuEmployeeList = driver.findElement(By.id("menu_pim_viewEmployeeList"));
 			subMenuEmployeeList.click();
 
 			System.out.println("Print PIM -> Employee List URL : " + driver.getCurrentUrl());
 
-			
-//	8.   Get count of total employees shown in the table and store in variable originalCount
-			
+			// 8.   Get count of total employees shown in the table and store in
+			// variable originalCount
+
 			WebElement clickSearch = driver.findElement(By.id("searchBtn"));
 			clickSearch.click();
-			
-			//Verify search results
-			
+
+			// Verify search results
+
 			WebElement tblElement = driver.findElement(By.id("resultTable"));
-					
+
 			WebElement tbodyElement = tblElement.findElement(By.tagName("tbody"));
-					
+
 			List<WebElement> trList = tbodyElement.findElements(By.tagName("tr"));
-					
+
 			int originalCount = trList.size();
 
-//	9.   Print the Count.
-			
-			System.out.println("Total original search Results: " + originalCount);		
-			
-//	10.  Using an employee ID, click on checkbox in the result table in front of empid row
-//	11.  Click Delete button	
-			
+			// 9. Print the Count.
+
+			System.out.println("Total original search Results: " + originalCount);
+
+			// 10. Using an employee ID, click on checkbox in the result table
+			// in front of empid row
+			// 11. Click Delete button
+
 			WebElement clickCheckBox = driver.findElement(By.id("ohrmList_chkSelectRecord_11"));
 			clickCheckBox.click();
-			
+
 			WebElement deleteBtn = driver.findElement(By.id("btnDelete"));
 			deleteBtn.click();
 
-//	12.  On confirmation dialog (Note:  This isn't an ALERT, you can inspect it) click OK button.
-//	13.  Verify that Successfully Deleted momentary popup appears.
-//	14.  Wait for popup to disappear.
-			
+			// 12. On confirmation dialog (Note: This isn't an ALERT, you can
+			// inspect it) click OK button.
+			// 13. Verify that Successfully Deleted momentary popup appears.
+			// 14. Wait for popup to disappear.
+
 			WebElement dialogDeleteBtn = driver.findElement(By.id("dialogDeleteBtn"));
 			dialogDeleteBtn.click();
 
@@ -442,154 +443,159 @@ public class Week3EmployeeTests {
 
 			wait.until(ExpectedConditions
 					.invisibilityOfElementLocated(By.xpath("//div[contains(@class, 'message success fadable')]")));
-			
-//	15.  Get count of rows in the results table and store in finalCount variable (int)
-			
-			//Verify search results
-			
+
+			// 15. Get count of rows in the results table and store in
+			// finalCount variable (int)
+
+			// Verify search results
+
 			tblElement = driver.findElement(By.id("resultTable"));
-					
+
 			tbodyElement = tblElement.findElement(By.tagName("tbody"));
-					
+
 			trList = tbodyElement.findElements(By.tagName("tr"));
-					
+
 			int finalCount = trList.size();
 
-//16.   Print finalCount.
-			
-			System.out.println("Total final search Results: " + finalCount);	
-			
-//17.  If finalCount+1 is equal to originalCount, print Passed, otherwise print failed.	
-			
-			if((finalCount+1)== originalCount){
-				
+			// 16. Print finalCount.
+
+			System.out.println("Total final search Results: " + finalCount);
+
+			// 17. If finalCount+1 is equal to originalCount, print Passed,
+			// otherwise print failed.
+
+			if ((finalCount + 1) == originalCount) {
+
 				System.out.println("Delete Employee Test Passed");
-				
+
 			} else {
-				
+
 				System.out.println("Delete Employee Test Failed");
-				
+
 			}
 
-//18.  Also verify that employee id deleted just now isn't shown in the table anymore.	
-			
-			for(int i=0;i<trList.size();i++){
-				
+			// 18. Also verify that employee id deleted just now isn't shown in
+			// the table anymore.
+
+			for (int i = 0; i < trList.size(); i++) {
+
 				List<WebElement> tdList = trList.get(i).findElements(By.tagName("td"));
-				
-				for( int j=1;j<tdList.size();j++){
-					
-										
-					if(tdList.get(j).getText(). equals ("0011"))
-					{
+
+				for (int j = 1; j < tdList.size(); j++) {
+
+					if (tdList.get(j).getText().equals("0011")) {
 						System.out.println("The Employee deletion failed");
-						
-					}
-					else{
+
+					} else {
 						System.out.println("The Employee is deleted successfully");
 						break;
 					}
-				 }
-			 }
-	
+				}
+			}
+
 		} else {
-			
+
 			System.out.println("Logon Failed");
-			
+
 		}
 	}
 
-	public void downloadEmpImportFile() {
-		
+	public void downloadEmpImportFile() throws InterruptedException {
 
-//		10.  Click upload to upload the same file again.
-//		11.  Quit the browser
-	    //Create FireFox Profile object
-		FirefoxProfile profile = new FirefoxProfile();
-        
-        //Set Location to store files after downloading.
-		profile.setPreference("browser.download.dir", "C:\\Users\\Abishek\\Documents");
-		profile.setPreference("browser.download.folderList", 2);
-        
-		profile.setPreference("browser.helperApps.neverAsk.saveToDisk", 
-			    " text/csv;"); 
-		
-		profile.setPreference( "browser.download.manager.showWhenStarting", false );
-		profile.setPreference( "pdfjs.disabled", true );
+		// 1.   Launch Browser.
+		// 2.   Open http://opensource.demo.orangehrmlive.com/
+		// 3.   Enter username - Admin
+		// 4.   Enter password - admin
+		// 5.   Click Login Button
+		//
+		// WebDriver driver = new FirefoxDriver(profile);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 
-//1.   	Launch Browser.
-//2.   	Open http://opensource.demo.orangehrmlive.com/
-//3.   	Enter username - Admin
-//4.   	Enter password - admin
-//5.   	Click Login Button
-				
-			WebDriver driver = new FirefoxDriver(profile);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-	
-			driver.get(url);
-			WebElement Username = driver.findElement(By.id(ID1));
-			Username.clear();
-			Username.sendKeys(userName);
-	
-			WebElement Password = driver.findElement(By.id(ID2));
-			Password.sendKeys(passWord);
-	
-			WebElement Login = driver.findElement(By.id(ID3));
-			Login.click();
-	
-// 6.   Verify that login succeeds and we then go to the PIM Page.
-	
-			actualHomePageText = driver.findElement(By.id("welcome")).getText();
-	
-			if (actualHomePageText.equals(expectedHomePageText)) {
-	
-				System.out.println("Logon Successful");
-	
-				WebElement tabPIM = driver.findElement(By.linkText("PIM"));
-				tabPIM.click();
-				
-//7.   Hover mouse over Configuration SubMenu and Click Data Import		
-				
-				WebElement subMenuConfiguration = driver.findElement(By.id("menu_pim_Configuration"));
-				
-				Actions action = new Actions(driver);
-				 
-			    action.moveToElement(subMenuConfiguration).build().perform();
-			 
-		        driver.findElement(By.linkText("Data Import")).click();
-			 
-//8.   Click Download Link on CSV Data Import Page
-//9.   Verify that file has been downloaded to the computer.
-		 		 
-				// Click to download 
-				
-		        driver.findElement(By.linkText("Download")).click();				
-				
-			} else {
-				
-				System.out.println("Logon Failed");
-				
+		driver.get(url);
+		WebElement Username = driver.findElement(By.id(ID1));
+		Username.clear();
+		Username.sendKeys(userName);
+
+		WebElement Password = driver.findElement(By.id(ID2));
+		Password.sendKeys(passWord);
+
+		WebElement Login = driver.findElement(By.id(ID3));
+		Login.click();
+
+		// 6.   Verify that login succeeds and we then go to the PIM Page.
+
+		actualHomePageText = driver.findElement(By.id("welcome")).getText();
+
+		if (actualHomePageText.equals(expectedHomePageText)) {
+
+			System.out.println("Logon Successful");
+
+			WebElement tabPIM = driver.findElement(By.linkText("PIM"));
+			tabPIM.click();
+
+			// 7.   Hover mouse over Configuration SubMenu and Click Data Import
+
+			WebElement subMenuConfiguration = driver.findElement(By.id("menu_pim_Configuration"));
+
+			Actions action = new Actions(driver);
+
+			action.moveToElement(subMenuConfiguration).build().perform();
+
+			driver.findElement(By.linkText("Data Import")).click();
+
+			// 8.   Click Download Link on CSV Data Import Page
+			// 9. Verify that file has been downloaded to the computer.
+
+			// Click to download
+
+			driver.findElement(By.linkText("Download")).click();
+			Thread.sleep(1000);
+
+			// Verify file download
+			String downloadPath = "C:\\Users\\Abishek\\Downloads";
+			File dir = new File(downloadPath);
+			File[] dirContents = dir.listFiles();
+
+			for (int i = 0; i < dirContents.length; i++) {
+				if (dirContents[i].getName().equals("importData.csv"))
+					System.out.println("File downloaded successfully");
 			}
-		
+
+			// 10. Click upload to upload the same file again.
+			// 11. Quit the browser
+
+			WebElement browseFile = driver.findElement(By.id("pimCsvImport_csvFile"));
+			Thread.sleep(1000);
+			browseFile.sendKeys("C:\\Users\\Abishek\\Downloads\\importData.csv");
+			Thread.sleep(1000);
+
+			WebElement uploadClick = driver.findElement(By.id("btnSave"));
+			uploadClick.click();
+
+		} else {
+
+			System.out.println("Logon Failed");
+
+		}
+
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		Week3EmployeeTests empTest = new Week3EmployeeTests();
 
-		//empTest.addEmployeeTest();
+		// empTest.addEmployeeTest();
 
-		//empTest.editEmployeeDOBTest();
+		// empTest.editEmployeeDOBTest();
 
-		//empTest.uploadEmployeeImage();
+		// empTest.uploadEmployeeImage();
 
-		//empTest.deleteEmployeeTest();
-		
+		// empTest.deleteEmployeeTest();
+
 		empTest.downloadEmpImportFile();
 
 	}
-
-
 
 }
