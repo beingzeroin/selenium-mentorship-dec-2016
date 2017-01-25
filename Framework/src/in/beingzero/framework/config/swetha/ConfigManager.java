304b1bae08895eff534de3122a99d2f2803b1eb6
sswethajain <sswethajain@gmail.com>
2017-01-25 21:49:05 +0000

    test

--- Framework/src/in/beingzero/framework/config/swetha/ConfigManager.java
+++ Framework/src/in/beingzero/framework/config/swetha/ConfigManager.java
@@ -1,5 +1,16 @@
 package in.beingzero.framework.config.swetha;
 
-public class ConfigManager {
+import org.testng.annotations.Test;
 
+@Test
+public class ConfigManager {
+	
+	public static void main(String[] args) throws InterruptedException {
+	
+	String configfilePath = System.getProperty("user.dir");
+			//.concat("/frameworkConfigfile.txt");
+	
+	System.out.println(configfilePath);
+
+	}
 }
