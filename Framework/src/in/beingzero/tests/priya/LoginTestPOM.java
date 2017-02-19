99251925b06b2b64da73a9750423f2521c15df05
priya <priyataran@gmail.com>
2017-02-19 08:00:13 +0530

    changed the log4j properties as discussed in the last meeting

--- Framework/src/in/beingzero/tests/priya/LoginTestPOM.java
+++ Framework/src/in/beingzero/tests/priya/LoginTestPOM.java
@@ -1,5 +1,6 @@
 package in.beingzero.tests.priya;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import in.beingzero.framework.POM.Priya.AddEmployeePage;
@@ -14,7 +15,7 @@ public class LoginTestPOM {
 	LoginPageOHRM loginPage;
 	AddEmployeePage AddEmpObj;
 	
-	@Test
+	@BeforeMethod
 	public void setUp()
 	{
 		loginPage = new LoginPageOHRM();
@@ -43,10 +44,10 @@ public class LoginTestPOM {
 	}*/
 	
 	
-	@Test(dataProvider = "loginTestData",dataProviderClass = AddEmpMulDataExcel.class) 
+	@Test(dependsOnMethods={"verifyLoginPageOHRM"},dataProvider = "loginTestData",dataProviderClass = AddEmpMulDataExcel.class) 
 	public void addEmployeeTest(String firstname,String middlename,String lastname)
 	{
-		System.out.println(firstname+middlename+lastname);
+	//	System.out.println(firstname+middlename+lastname);
 		PersonalDetails vfemp = AddEmpObj.addEmployeeDt(firstname,middlename,lastname);
 		vfemp.verifynewAddedEmp();
 		System.out.println("checking how many times this called");
