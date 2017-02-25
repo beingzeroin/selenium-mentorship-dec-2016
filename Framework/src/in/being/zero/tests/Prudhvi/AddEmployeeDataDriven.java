7151712b67c684cf1d9bf257a3671b70d0eca5ce
tammanap <tammanap@gmail.com>
2017-02-25 18:28:24 +0530

    Add Employee data drive test is added

---
+++ Framework/src/in/being/zero/tests/Prudhvi/AddEmployeeDataDriven.java
@@ -0,0 +1,45 @@
+package in.being.zero.tests.Prudhvi;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import in.beingzero.framework.POM.Prudhvi.AddEmployeePage;
+import in.beingzero.framework.POM.Prudhvi.DashBoardPage;
+import in.beingzero.framework.POM.Prudhvi.EmployeeListPage;
+import in.beingzero.framework.POM.Prudhvi.LoginPage;
+import in.beingzero.framework.config.Prudhvi.ConfigManager;
+import in.beingzero.framework.data.sandeep.DataManager;
+import in.beingzero.framework.data.sandeep.ExcelDataManager;
+
+public class AddEmployeeDataDriven {
+	
+	//@Test
+	public void LoginTest()
+	{
+		LoginPage objLoginPage=new LoginPage();
+		ConfigManager objCM=new ConfigManager();
+		DashBoardPage objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
+
+	}
+	
+	@Test(dataProvider="dp")//dependsOnMethods={"LoginTest"},
+	public void loginTest(String firstName, String middleName, String lastName)
+	{
+		
+		LoginPage objLoginPage=new LoginPage();
+		ConfigManager objCM=new ConfigManager();
+		DashBoardPage objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
+		AddEmployeePage objAddEmpPage=objDBPage.navigateToAddEmployeePage();
+		EmployeeListPage obj=objAddEmpPage.addEmployee(firstName,middleName,lastName);
+	}
+	
+	@DataProvider
+	public Object[][] dp()
+	{
+		DataManager dm = new ExcelDataManager("testdata/OrangeHRM-TestData.xlsx", "AuthTests", "EmployeeDetails");
+		return dm.getData();
+	}
+
+
+}
