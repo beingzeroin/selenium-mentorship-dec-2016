ae88033a50dd28ae14c93846edcd0ec31aa631f9
tammanap <tammanap@gmail.com>
2017-03-05 18:48:11 +0530

    Need to discuss

--- Framework/src/in/being/zero/tests/Prudhvi/AddEmployeeDataDriven.java
+++ Framework/src/in/being/zero/tests/Prudhvi/AddEmployeeDataDriven.java
@@ -1,10 +1,11 @@
 package in.being.zero.tests.Prudhvi;
 
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import in.beingzero.framework.POM.Prudhvi.AddEmployeePage;
+import in.beingzero.framework.POM.Prudhvi.BasePage;
 import in.beingzero.framework.POM.Prudhvi.DashBoardPage;
 import in.beingzero.framework.POM.Prudhvi.EmployeeListPage;
 import in.beingzero.framework.POM.Prudhvi.LoginPage;
@@ -13,26 +14,45 @@ import in.beingzero.framework.data.sandeep.DataManager;
 import in.beingzero.framework.data.sandeep.ExcelDataManager;
 
 public class AddEmployeeDataDriven {
-	
-	//@Test
+	 DashBoardPage objDBPage;
+	 BasePage objBasePage;
+	@BeforeMethod
 	public void LoginTest()
 	{
 		LoginPage objLoginPage=new LoginPage();
 		ConfigManager objCM=new ConfigManager();
-		DashBoardPage objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
+		objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
 
 	}
 	
-	@Test(dataProvider="dp")//dependsOnMethods={"LoginTest"},
-	public void loginTest(String firstName, String middleName, String lastName)
+	@Test(dataProvider="dp"/*,dependsOnMethods={"LoginTest"}*/)
+	public void AddEmployee(String firstName, String middleName, String lastName)
 	{
 		
-		LoginPage objLoginPage=new LoginPage();
-		ConfigManager objCM=new ConfigManager();
-		DashBoardPage objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
-		AddEmployeePage objAddEmpPage=objDBPage.navigateToAddEmployeePage();
-		EmployeeListPage obj=objAddEmpPage.addEmployee(firstName,middleName,lastName);
+		//LoginPage objLoginPage=new LoginPage();
+		//ConfigManager objCM=new ConfigManager();
+		//DashBoardPage objDBPage=objLoginPage.validLogin(objCM.getProperty("userName"), objCM.getProperty("password"));
+		//DashBoardPage objDBPage =new DashBoardPage();
+		AddEmployeePage objAddEmpPage=objBasePage.NavigateToAddEmployee1();
+		EmployeeListPage objEmployeeList=objAddEmpPage.addEmployee(firstName,middleName,lastName);
+		objEmployeeList.verifyPersonalDetails(firstName, middleName, lastName);
 	}
+	//@Test
+	public void AddMultipleEmployees()
+	{
+		Object[][] Employees=dp();
+		int iEmployeeNo=Employees.length;
+		
+		for(int i=0;i<iEmployeeNo;i++)
+		{			
+			AddEmployeePage objAddEmpPage=objDBPage.navigateToAddEmployeePage();
+			EmployeeListPage obj=objAddEmpPage.addEmployee(Employees[i][0].toString(),Employees[i][1].toString(),Employees[i][2].toString());
+		}
+		
+	}
+	
+	
+	
 	
 	@DataProvider
 	public Object[][] dp()
