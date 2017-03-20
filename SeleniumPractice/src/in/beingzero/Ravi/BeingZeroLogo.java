3612b5b8584a495a6ca71da677c6b6cb00742f1e
Ravi <ravi.dubey13@gmail.com>
2017-03-20 22:53:23 +1100

    changes

--- SeleniumPractice/src/in/beingzero/Ravi/BeingZeroLogo.java
+++ SeleniumPractice/src/in/beingzero/Ravi/BeingZeroLogo.java
@@ -3,12 +3,12 @@ package in.beingzero.Ravi;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BeingZeroLogo {
 
 	public static void main(String[] args) throws Exception {
-		WebDriver ch = new ChromeDriver();
+		WebDriver ch = new FirefoxDriver();
 		ch.get("http://beingzero.in/");
 		Thread.sleep(4000);
 		WebElement logo = ch.findElement(By.xpath("//div[@id='branding']//img[@class='preload-me']"));
